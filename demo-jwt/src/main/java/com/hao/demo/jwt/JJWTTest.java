028e26933aeba33f434246275458088961830ac1
zhenghao <zh@alicms.com>
2019-12-18 16:06:08 +0800

    update

--- demo-jwt/src/main/java/com/hao/demo/jwt/JJWTTest.java
+++ demo-jwt/src/main/java/com/hao/demo/jwt/JJWTTest.java
@@ -1,12 +1,12 @@
 package com.hao.demo.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import org.apache.tomcat.util.codec.binary.Base64;
 
-import java.util.Calendar;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -15,38 +15,68 @@ import java.util.Map;
  * @Date 2019/12/18 14:56
  */
 public class JJWTTest {
-    private static String SECRET = "DyoonSecret_0581";
-
-    private static String getJwtToken(Long userId){
-        Date iatDate = new Date();
-        // expire time
-        Calendar nowTime = Calendar.getInstance();
-        nowTime.add(Calendar.DATE, 10);
-        Date expiresDate = nowTime.getTime(); //有10天有效期
-        Claims claims = Jwts.claims();
-        claims.put("name","cy");
-        claims.put("userId", userId);
-        claims.setAudience("cy");
-        claims.setIssuer("cy");
-        return Jwts.builder().setClaims(claims).setExpiration(expiresDate).signWith(SignatureAlgorithm.HS512, SECRET).compact();
-    }
-
-    public static Jws<Claims> parseJwtToken(String token) {
-        Jws<Claims> jws = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
-        String signature = jws.getSignature();
-        Map<String, String> header = jws.getHeader();
-        Claims Claims = jws.getBody();
-        return jws;
-    }
-
     public static void main(String[] args) {
-        String jwtToken = getJwtToken(222L);
-        System.out.println("token:" + jwtToken);
-        Jws<Claims> claimsJws = parseJwtToken(jwtToken);
-        System.out.println("claimsJws:" + claimsJws);
+        String token = getJwtToken("abcdefg", 132L);
+        System.out.println("token:" + token);
+
+        Jws<Claims> claimsJws = verifyJwt(token);
+        System.out.println("claims:" + claimsJws);
         System.out.println("signature:" + claimsJws.getSignature());
         System.out.println("header:" + claimsJws.getHeader());
         System.out.println("Claims:" + claimsJws.getBody());
+    }
+
+
+
+    // token 过期时间, 单位: 秒. 这个值表示 30 天
+    private static final long TOKEN_EXPIRED_TIME = 30 * 24 * 60 * 60;
+    // jwt 加密解密密钥
+    private static final String JWT_SECRET = "MDk4ZjZiY2Q0NjIxZDM3M2NhZGU0ZTgzMjYyN2I0ZjY=";
+    /**
+     * 根据userId和openid生成token
+     */
+    public static String getJwtToken(String openId, Long userId) {
+        /*Claims claims = Jwts.claims();
+        claims.put("name","cy");
+        claims.put("userId", userId);
+        claims.setAudience("cy");
+        claims.setIssuer("cy");*/
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("openId", openId);
+        map.put("userId", userId);
+
+        Date now = new Date();
+        //下面就是在为payload添加各种标准声明和私有声明了
+        JwtBuilder builder = Jwts.builder() // 这里其实就是new一个JwtBuilder，设置jwt的body
+                .setClaims(map)             // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
+                .setId("tokenId")           // 设置jti(JWT ID)：是JWT的唯一标识，根据业务需要，这个可以设置为一个不重复的值，主要用来作为一次性token,从而回避重放攻击。
+                .setAudience("PC")          // aud: 接收jwt的一方
+                .setIssuer("user_id")       // iss: jwt签发者
+//                .setSubject("")           // sub: jwt所面向的用户
+//                .setNotBefore(now)          // nbf: 定义在什么时间之前，该jwt都是不可用的.
+                .setIssuedAt(now)           // iat: jwt的签发时间
+                .setExpiration(new Date(now.getTime() + TOKEN_EXPIRED_TIME))    //设置过期时间
+                .signWith(SignatureAlgorithm.HS256, generalKey());     //设置签名使用的签名算法和签名使用的秘钥，此处是header那部分，jjwt已经将这部分内容封装好了。
+        return builder.compact();
+    }
+
+
+    /**
+     * 解析jwt
+     */
+    public static Jws<Claims> verifyJwt(String token) {
+        return Jwts.parser()        //得到DefaultJwtParser
+                .setSigningKey(generalKey()) //设置签名的秘钥
+                .parseClaimsJws(token);
+
+    }
+
+    /**
+     * 由字符串生成加密key
+     */
+    public static SecretKey generalKey() {
+        return new SecretKeySpec(Base64.decodeBase64(JWT_SECRET), "AES");
     }
 
 }
