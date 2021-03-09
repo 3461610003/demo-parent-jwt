ad14b4c03af1de0a5cfc4d776021aae7cc1c4e3c
zhenghao <zh@alicms.com>
2021-03-09 16:35:19 +0800

    commit all

---
+++ demo-shiro-simplify/src/main/java/com/hao/demo/shiro/simplify/DemoShiroSimplifyApplication.java
@@ -0,0 +1,13 @@
+package com.hao.demo.shiro.simplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoShiroSimplifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoShiroSimplifyApplication.class, args);
+    }
+
+}
---
+++ demo-shiro-simplify/src/test/java/com/hao/demo/shiro/simplify/DemoShiroSimplifyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hao.demo.shiro.simplify;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class DemoShiroSimplifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
